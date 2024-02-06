b3d9253c5ea464b6c8383e338c58c8690447760f
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2024-02-06 21:14:43 +0700

    clear home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,16 +2,8 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex flex-row justify-evenly items-center m-6">
-      <div className="bg-gray-400 flex flex-col p-6 rounded-md">
-        Total Invoices
-      </div>
-      <div className="bg-green-400 flex flex-col p-6 rounded-md">
-        Paid Invoices
-      </div>
-      <div className="bg-red-400 flex flex-col p-6 rounded-md">
-        Unpaid Invoices
-      </div>
+    <div>
+      
     </div>
   );
 };
